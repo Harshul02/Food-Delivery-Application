122fe9532be3499409f63b8b542f4dab63b46a13
Harshul Jain <harshulgangwal02@gmail.com>
2023-01-30 10:30:31 +0530

    Added Card Component

--- foodapp/src/App.js
+++ foodapp/src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import Home from './screens/Home';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Login from './screens/Login';
 
 function App() {
   return (
     <>
+    <BrowserRouter>
     <div>
-      <Home />
+    <Routes>
+    <Route exact path="/" element={<Home />}></Route>
+    <Route exact path="/login" element={<Login />}></Route>
+    </Routes>
     </div>
+    </BrowserRouter>
     </>
   );
 }
--- foodapp/src/screens/Home.jsx
+++ foodapp/src/screens/Home.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import React from "react";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Card from "../components/Card";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
     <div>
-        <div><Navbar /></div>
-        <div>Body</div>
+      <div>
+        <Navbar />
+      </div>
+      <div><Card /></div>
+      <div>
+        <Footer />
+      </div>
     </div>
-  )
+  );
 }
